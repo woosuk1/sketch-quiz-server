123ef87f890f909a215a135bc1a500b10e830a63
giuseog <giuseog@gmail.com>
2025-06-02 16:40:49 +0900

    feat: 친구 entity & 패키지 생성

--- src/main/java/com/itcen/whiteboardserver/friends/controller/FriendsController.java
+++ src/main/java/com/itcen/whiteboardserver/friends/controller/FriendsController.java
@@ -1,7 +1,26 @@
 package com.itcen.whiteboardserver.friends.controller;
 
+import com.itcen.whiteboardserver.friends.dto.FriendsDTO;
+import com.itcen.whiteboardserver.friends.service.FriendsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/friends")
+@RequiredArgsConstructor
 public class FriendsController {
+
+    private final FriendsService friendsService;
+
+    @PostMapping("/friends")
+     public ResponseEntity<FriendsDTO> postFriend(@RequestBody FriendsDTO friendsDTO) {
+
+        friendsService.postFriend(friendsDTO);
+
+        return ResponseEntity.ok(friendsDTO);
+     }
 }
--- src/main/java/com/itcen/whiteboardserver/friends/service/FriendsService.java
+++ src/main/java/com/itcen/whiteboardserver/friends/service/FriendsService.java
@@ -1,4 +1,7 @@
 package com.itcen.whiteboardserver.friends.service;
 
+import com.itcen.whiteboardserver.friends.dto.FriendsDTO;
+
 public interface FriendsService {
+    void postFriend(FriendsDTO friendsDTO);
 }
