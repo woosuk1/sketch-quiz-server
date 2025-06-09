3edce9ecf552019f9f2921aac37aa88da39aa03e
giuseog <giuseog@gmail.com>
2025-06-09 10:02:27 +0900

    feat: OAuth2LoginSuccessHandlerTest 테스트 코드 작성

--- src/test/java/com/itcen/whiteboardserver/OidcLoginMockMvcTests.java
+++ src/test/java/com/itcen/whiteboardserver/OidcLoginMockMvcTests.java
@@ -1,62 +1,158 @@
-//package com.itcen.whiteboardserver;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.Map;
-//
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class OidcLoginMockMvcTests {
-//
-//    /* 설명.
-//     *  MockMvc 주입으로 application context 생성 및 필터 체인 동작
-//    * */
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    @DisplayName("OIDC 로그인 성공 후, 보호된 API에 접근하면 200 OK 반환")
-//    void oidcLogin_and_accessProtectedApi() throws Exception {
-//
-//        /* 설명. Mock user claims 정의(sub, email) */
-//        Map<String, Object> claims = Map.of(
-//                StandardClaimNames.SUB, "sub",
-//                StandardClaimNames.EMAIL, "test@example.com",
-//                "nickname",               "테스트유저"
-//        );
-//
-//        /* 설명. MockMvc 요청 시, with(oidcLogin())으로 인증 정보 모킹 */
-//        mockMvc.perform(get("/api/member")
-//                .with(oidcLogin()
-//                        .idToken(id -> id
-//                                .claim(StandardClaimNames.SUB, claims.get(StandardClaimNames.SUB))
-//                                .claim(StandardClaimNames.EMAIL, claims.get(StandardClaimNames.EMAIL))
-//                        )
-//                        .userInfoToken(attrs -> {
-//                            attrs.put("nickname", claims.get("nickname"));
-//                        })
-//                )
-//            )
-//                /* 설명.
-//                 *  Oauth2LoginSuccessHandler가 CustomPrincipal로 교체하면
-//                 *  controller가 받아서 정상 memberResponseDTO 반환
-//                * */
-//                .andExpect(status().isOk())
-//                /* 설명. 응답값들이 return 되는지 검증 */
-//                .andExpect(jsonPath("$.email").value("test@example.com"))
-//                .andExpect(jsonPath("$.nickname").value("
-//    }
-//
-//
-//}
+package com.itcen.whiteboardserver;
+
+import com.itcen.whiteboardserver.auth.service.TokenService;
+import com.itcen.whiteboardserver.member.dto.MemberDTO;
+import com.itcen.whiteboardserver.member.entity.Member;
+import com.itcen.whiteboardserver.member.enums.AuthProvider;
+import com.itcen.whiteboardserver.member.enums.MemberRole;
+import com.itcen.whiteboardserver.member.repository.MemberRepository;
+import com.itcen.whiteboardserver.member.service.MemberService;
+import com.itcen.whiteboardserver.member.service.MemberServiceImpl;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.security.oauth2.core.oidc.StandardClaimNames.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
+class OidcLoginMockMvcTests {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired MemberRepository memberRepository;
+    @Autowired PasswordEncoder passwordEncoder;
+    @Autowired private MemberServiceImpl memberServiceImpl;
+
+    @MockitoBean TokenService tokenService;
+
+    private Member saved;
+
+    @Test
+    @DisplayName("OIDC 로그인 성공 시,  custom principal 과 토큰 발급 흐름 검증")
+    void oidcLogin_createsCustomPrincipal_andIssuesCookies() throws Exception {
+
+        // --- Given ---
+        // 테스트용 회원을 DB에 저장
+        memberRepository.deleteAll();
+        Member member = Member.builder()
+                .email("test@kakao.com")
+                .password(passwordEncoder.encode("irrelevant"))
+                .nickname("코난")
+                .memberRole(Collections.singleton(MemberRole.MEMBER))
+                .provider(AuthProvider.KAKAO)
+                .providerId("kakao-user-123")
+                .build();
+        saved = memberRepository.saveAndFlush(member);
+
+        System.out.println("Saved Member: " + saved.getId());
+        // 1) DB에서 조회한 MemberDTO
+//        MemberDTO dbMember = MemberDTO.builder()
+//                .id(12L)
+//                .email("test@kakao.com")
+//                .nickname("코난")
+//                .memberRole(Set.of(MemberRole.MEMBER))
+//                .build();
+//        given(memberServiceImpl.getMemberByEmail("test@kakao.com")).willReturn(dbMember);
+        MemberDTO dbMember = memberServiceImpl.getMemberByEmail("test@kakao.com");
+
+        System.out.println("DB Member: " + dbMember.toString());
+
+        // 2) TokenService 에서 발급할 쿠키
+        ResponseCookie access  = ResponseCookie.from("access_token",  "AAA").build();
+        ResponseCookie refresh = ResponseCookie.from("refresh_token", "RRR").build();
+        given(tokenService.issueTokens(
+//                eq("test@kakao.com"),
+//                eq("코난"),
+//                eq("12"),
+//                eq(List.of("MEMBER"))
+                eq(dbMember.getEmail()),
+                eq(dbMember.getNickname()),
+                eq(String.valueOf(dbMember.getId())),
+                eq(List.of(MemberRole.MEMBER.name()))
+        )).willReturn(new ResponseCookie[]{access, refresh});
+
+
+        // 3) 임의 ClientRegistration (registrationId=kakao)
+        ClientRegistration kakao = ClientRegistration.withRegistrationId("kakao")
+                .clientId("dummyId")
+                .clientSecret("dummySecret")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "account-email")
+                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                .tokenUri("https://kauth.kakao.com/oauth/token")
+                .jwkSetUri("https://kauth.kakao.com/oauth/jwks")
+                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                .userNameAttributeName("email")
+                .clientName("Kakao")
+                .build();
+
+        // --- When & Then ---
+        mockMvc.perform(get("/api/member")
+                        .with(oidcLogin()
+                                .clientRegistration(kakao)
+                                // ID 토큰에 sub, email 넣기
+                                .idToken(token -> token
+                                        .claim("sub", "kakao-user-123")
+                                        .claim("email", "test@kakao.com")
+                                )
+                                // UserInfo 에 nickname 넣기
+                                .userInfoToken(ui -> ui.claim("nickname", "코난"))
+                                // 권한은 ROLE_USER
+                                .authorities(new SimpleGrantedAuthority("ROLE_USER"))
+                        )
+                )
+                .andExpect(status().isOk())
+                .andExpect(cookie().value("access_token",  "AAA"))
+                .andExpect(cookie().value("refresh_token", "RRR"))
+                // 컨트롤러가 리턴한 JSON 에 custom principal 정보가 포함되어야 함
+                .andExpect(jsonPath("$.id").value(35))
+                .andExpect(jsonPath("$.email").value("test@kakao.com"))
+                .andExpect(jsonPath("$.nickname").value("코난"));
+    }
+
+    @Test
+    @DisplayName("익명(비로그인) 시 /api/member 호출, 404 Not Found + MEMBER_NOT_FOUND 에러 코드 호출")
+    void givenAnonymous_whenGetMember_thenNotFound() throws Exception {
+        mockMvc.perform(get("/api/member"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value(40401 ))
+                .andExpect(jsonPath("$.message").value("존재하지 않는 회원입니다."));
+    }
+}
